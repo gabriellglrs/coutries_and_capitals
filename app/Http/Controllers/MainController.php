d7fa7dcc1e3219de32c27ae5d550d66af89c2935
gabriellglrs <gabriellglrs@gmail.com>
2025-09-26 23:14:49 -0300

    feat(game): Adiciona tela inicial e preparação do jogo
    Este commit introduz a funcionalidade inicial para o jogo de quiz.
    
    As principais alterações incluem:
    - Criação da rota e view para a tela de início, onde o usuário pode definir o número de perguntas.
    - Adição da rota POST para lidar com a submissão do formulário e iniciar a preparação do quiz.
    - Implementação da validação para o número de perguntas no MainController.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
@@ -14,8 +16,47 @@ class MainController extends Controller
         $this->appData = require(app_path('appData.php'));
     }
 
-    public function showData()
+    public function startGame(): View
     {
-        return response()->json($this->appData);
+        return view('home');
+    }
+
+    public function prepareGame(Request $request)
+    {
+        // Validar request
+        $validated = $request->validate(
+            [
+                'total_questions' => 'required|integer|min:3|max:30',
+            ],
+            [
+                'total_questions.required' => 'O número de questões é obrigatório.',
+                'total_questions.integer'  => 'O número de questões precisa ser um valor inteiro.',
+                'total_questions.min'      => 'O mínimo permitido é :min questões.',
+                'total_questions.max'      => 'O máximo permitido é :max questões.',
+            ]
+        );
+
+        // get total questions
+        $total_questions = intval($request->input('total_questions'));
+        $quiz = $this->prepareQuiz($total_questions);
+
+        dd($quiz);
+    }
+
+    private function prepareQuiz(int $totalQuestions): array
+    {
+        /*  $countries = $this->appData['countries'];
+        shuffle($countries);
+
+        $quiz = [];
+        for ($i = 0; $i < $totalQuestions; $i++) {
+            $country = array_shift($countries);
+            $quiz[] = [
+                'country' => $country['country'],
+                'capital' => $country['capital'],
+            ];
+        }
+
+        return $quiz; */
     }
 }
