a372f4f5dc8ab37575a6868d870a4ec4bc88a239
gabriellglrs <gabriellglrs@gmail.com>
2025-09-26 22:40:35 -0300

    feat: Adiciona a página inicial e componentes de layout
    Este commit introduz a página inicial da aplicação, juntamente com os componentes de layout necessários para a sua renderização.
    
    - Cria a view `Home.blade.php` como a página principal.
    - Adiciona os componentes Blade `MainLayout`, `footer` e `logo` para estruturar a página.
    - Atualiza o arquivo de rotas `web.php` para direcionar a rota raiz ('/') para a nova view `home`.
    - Remove a rota de exemplo que retornava a view `welcome`.

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,3 @@
+<x-main-layout pageTitle="Countries and Capitals Quiz">
+    <h1 class="text-center my-5">home page</h1>
+</x-main-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
+
 Route::get('/show', [MainController::class, 'showData']);
